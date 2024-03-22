9949a4459acb6221644502fd1f7900413cf0afab
Areum <158556098+Areum123123@users.noreply.github.com>
2024-03-22 22:00:41 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -0,0 +1,54 @@
+function solution(n) {
+    if(n <= 7){
+        return 1;
+    }
+     else if(n <= 14){
+        return 2;
+    }
+     else if(n <= 21){
+        return 3;
+    }
+    
+     else if(n <= 28){
+        return 4;
+    }
+    
+     else if(n <= 35){
+        return 5;
+    }
+    
+    else if(n <= 42){
+        return 6;
+    }
+    
+     else if(n <= 49){
+        return 7;
+    }
+    
+    else if(n <= 56){
+        return 8;
+    }
+    
+    else if(n <= 63){
+        return 9;
+    }
+    else if(n <= 70){
+        return 10;
+    }
+    else if(n <= 77){
+        return 11;
+    }
+    else if(n <= 84){
+        return 12;
+    }
+    else if(n <= 91){
+        return 13;
+    }
+    else if(n <= 98){
+        return 14;
+    }
+    else if(n <= 100){
+        return 15;
+    }
+    
+}
