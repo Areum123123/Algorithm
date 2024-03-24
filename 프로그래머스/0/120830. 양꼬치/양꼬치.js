e55befb038b525ffe30bf9c9cb12d009b2cf8682
Areum <158556098+Areum123123@users.noreply.github.com>
2024-03-24 19:47:07 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.js
@@ -3,10 +3,10 @@ let  skewerPrice = n * 12000;
 let drinkPrice = k * 2000;
 let discountN = Math.floor(n * 0.1)
 let answer =  skewerPrice + drinkPrice 
-if(n < 10){
-  return  answer;
-} else if(n >= 10){
+if(n >= 10){
     return  answer -(2000 * discountN);;
+} else {
+    return answer;
 }
 }
 
