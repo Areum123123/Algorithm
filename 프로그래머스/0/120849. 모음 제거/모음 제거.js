bb43707d32cb64865da7e6edc6e3792a29e136ed
Areum <158556098+Areum123123@users.noreply.github.com>
2024-04-08 15:21:35 +0900

    [level 0] Title: 모음 제거, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -0,0 +1,10 @@
+const vowels = [ 'a', 'e', 'i', 'o', 'u'];
+function solution(my_string) {
+   let result = '';
+    for(let i=0; i < my_string.length; i++){
+     if(!vowels.includes(my_string[i])){
+          result += my_string[i]
+     }       
+   }
+    return result;
+}
