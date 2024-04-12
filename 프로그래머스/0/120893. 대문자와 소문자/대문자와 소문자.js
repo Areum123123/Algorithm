d456a53dc5c7f70434a84b30b754b75d5f114ab0
Areum <158556098+Areum123123@users.noreply.github.com>
2024-04-12 14:18:24 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -0,0 +1,11 @@
+function solution(my_string) {
+    let result = '';
+   for(let i=0; i<my_string.length; i++){
+     if(my_string[i] === my_string[i].toLowerCase()){
+         result += my_string[i].toUpperCase();
+     } else {
+         result += my_string[i].toLowerCase();
+     }
+   }
+    return result;
+}
