a75ca362fd45b87d0b64234f891ab543bec6f01e
Areum <158556098+Areum123123@users.noreply.github.com>
2024-04-14 01:22:58 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -2,6 +2,6 @@ function solution(array) {
    let maxNum = Math.max(...array);
    let indexNum = array.indexOf(maxNum);
     
-   array = [maxNum, indexNum];
-    return array;
+   
+    return [maxNum, indexNum];
 }
