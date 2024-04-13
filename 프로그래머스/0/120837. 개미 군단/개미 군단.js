b84928c03a479ee9ddb46cda19aae97a5b3bd910
Areum <158556098+Areum123123@users.noreply.github.com>
2024-04-13 19:22:04 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -1,14 +1,11 @@
 function solution(hp) {
-    let result = 0;
+   
     let a = Math.floor(hp / 5);
     let b = Math.floor((hp % 5) / 3);
-    let c = ((hp % 5) % 3) / 1
-    if(hp >= 0){
-      result += a
-      result += b 
-      result += c
-    }
-  return result;   
+    let c = ((hp % 5) % 3) / 1;
+
+    
+  return a + b + c;   
  }
 
 
