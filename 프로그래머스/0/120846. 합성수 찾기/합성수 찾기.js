b64faaf64b668b96088eb4c4d904d6c6cdc050c3
Areum <158556098+Areum123123@users.noreply.github.com>
2024-04-24 11:22:23 +0900

    [level 0] Title: 합성수 찾기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
@@ -2,7 +2,7 @@ function solution(n) {
     let count = 0;
     for(let i=4; i <= n; i++){
         let a = false;
-      for(let j=2; j <= Math.sqrt(i); j++){
+      for(let j=2; j <= n; j++){
           if(i % j === 0 && i!==j){
               a =true;
               break;
