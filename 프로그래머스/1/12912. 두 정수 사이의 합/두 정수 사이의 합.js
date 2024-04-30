93c1dbb445950f849c141cc61494cd0faf9a181f
Areum <158556098+Areum123123@users.noreply.github.com>
2024-04-30 09:21:35 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -1,16 +1,18 @@
-function solution(a, b){
-    let result =0;
-    
-    if(a === b){
-     result =a;    
-    } else if(a < b){
-    for(let i = a; i <= b; i++){
-       result += i;
-    } 
-    }else {
-        for(let i = b; i <= a; i++){
-       result += i;
-          }    
-       }
+function solution(a, b) {
+    let result = 0;
+
+    if (a === b) {
+        result = a;
+
+    } else if (a < b) {
+        for (let i = a; i <= b; i++) {
+            result += i;
+        }
+
+    } else {
+        for (let i = b; i <= a; i++) {
+            result += i;
+        }
+    }
     return result;
 }
