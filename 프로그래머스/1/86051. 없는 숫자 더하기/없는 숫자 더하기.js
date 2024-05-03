28ca99b7a73f4074b9b4033ed67261693442fefc
Areum <158556098+Areum123123@users.noreply.github.com>
2024-05-03 09:30:25 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,7 +1,6 @@
 function solution(numbers) {
     let answer =0;
-    let newArr = numbers.sort((a,b)=>a-b)
-    console.log(newArr)
+   
     for(let i=0; i<10; i++){
         if(numbers.indexOf(i) === -1){
          answer += i
