f15ef49261d9368996e80f9bdba945b425267aff
Areum <158556098+Areum123123@users.noreply.github.com>
2024-05-04 21:10:22 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,11 +1,6 @@
 function solution(s) {
-    let result = '';
-    const middle = Math.floor(s.length/2)
-   if(s.length % 2 === 0){
-       result += s[middle-1]+s[middle];
-       
-   } else {
-       result += s[middle];     
-   }
-    return result;
+  
+    const middle = Math.floor(s.length/2);
+    
+   return s.length % 2 ? s[middle] : s[middle-1]+s[middle];
 }
