23d207377f269699383419b97ad1c082a0daa344
Areum <158556098+Areum123123@users.noreply.github.com>
2024-05-13 09:20:40 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,10 +1,4 @@
 function solution(t, p) {
-    let result = 0;
-   for(let i =0; i <=t.length-p.length; i++){
-  let part = t.slice(i, p.length +i);
-       if(part <= p){
-           result ++;
-       }
-   }
-    return result;
+    var answer = 0;
+    return answer;
 }
