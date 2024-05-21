af6b04f654a0bb63ab7bd0215f3adcf272027133
Areum <158556098+Areum123123@users.noreply.github.com>
2024-05-21 10:00:59 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,6 +1,6 @@
 function solution(food) {
     let result ='';
-    for(let i =0; i < food.length; i++){
+    for(let i =1; i < food.length; i++){
         if(food[i] > 1){
             let count = Math.floor(food[i]/2);
             result += String(i).repeat(count);
