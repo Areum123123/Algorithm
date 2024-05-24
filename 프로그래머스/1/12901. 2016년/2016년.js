03a80cd7cdf3dcf1b70a28b1b8b9d670e8e69a9c
Areum <158556098+Areum123123@users.noreply.github.com>
2024-05-24 09:46:24 +0900

    [level 1] Title: 2016년, Time: 0.11 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,8 +1,4 @@
-let day = ['SUN','MON', 'TUE', 'WED','THU', 'FRI', 'SAT'];
-
 function solution(a, b) {
-   let date = '2016-'+a +'-'+b;
-   let date1 = new Date(date).getDay()
-  return day[date1];
+    var answer = '';
+    return answer;
 }
-
