0eb085311854800416c4f5cf8af6784606d84a4f
Areum <kang10232@naver.com>
2024-06-05 09:33:16 +0900

    [level 1] Title: 콜라 문제, Time: 0.03 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -1,10 +1,9 @@
 function solution(a, b, n) {
     let result = 0;
-   let remainder = n % a
-   
+     
    while(n >= a){
        result += Math.floor(n/a)*b
-        n = Math.floor(n/a)*b +  n % a
+       n = Math.floor(n/a)*b +  n % a
    }
    return result;
 }
