f385e7a7e3b209aaddab0b43ec03ba21a26f74ce
Areum <kang10232@naver.com>
2024-06-07 09:45:23 +0900

    [level 1] Title: 2016년, Time: 0.09 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,4 +1,8 @@
+let day = ['SUN','MON', 'TUE', 'WED','THU', 'FRI', 'SAT'];
+
 function solution(a, b) {
-    var answer = '';
-    return answer;
+   // let date = '2016-'+a +'-'+b;
+   let date1 = new Date('2016-'+a +'-'+b).getDay()
+  return day[date1];
 }
+
