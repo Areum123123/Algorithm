1dc2751d11e82c2400095484038673b3a78a5a9c
Areum <kang10232@naver.com>
2024-06-09 20:47:37 +0900

    [level 0] Title: A로 B 만들기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,10 +1,9 @@
 function solution(before, after) {
     const arrayBefore = [...before].sort();
     const arrayAfter = [...after].sort();
-    console.log(arrayBefore)
-    console.log(arrayAfter)
+
     if(JSON.stringify(arrayBefore) === JSON.stringify(arrayAfter)){
-        return 1
+        return 1;
     }
-    return 0
+    return 0;
 }
