c983206df5a48b145e27706d12043d16581ef3db
Areum <kang10232@naver.com>
2024-06-10 09:38:50 +0900

    [level 1] Title: 모의고사, Time: 1.49 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,21 +1,31 @@
 function solution(answers) {
-    var answer = [];
-    const su1 = [1,2,3,4,5];
-    const su2 = [2,1,2,3,2,4,2,5];
-    const su3 = [3,3,1,1,2,2,4,4,5,5];
-    let sumarr = [0,0,0];
-  
-    for(let i=0; i<answers.length; i++){
-        if(answers[i]==su1[i%su1.length]) sumarr[0]++;
-        if(answers[i]==su2[i%su2.length]) sumarr[1]++;
-        if(answers[i]==su3[i%su3.length]) sumarr[2]++;
-    }
-    let maxValue = Math.max(...sumarr);
-    for(let j=0; j<sumarr.length; j++){
-        if(sumarr[j] == maxValue){
-            answer.push(j+1);
+    // 수포자들의 답안 패턴 정의
+    const su1 = [1, 2, 3, 4, 5];
+    const su2 = [2, 1, 2, 3, 2, 4, 2, 5];
+    const su3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    
+    // 각 수포자가 맞춘 문제 수를 저장할 배열
+    let scores = [0, 0, 0];
+    
+    // 답안을 순회하면서 각 수포자의 점수를 계산
+    answers.forEach((answer, index) => {
+        if (answer === su1[index % su1.length]) scores[0]++;
+        if (answer === su2[index % su2.length]) scores[1]++;
+        if (answer === su3[index % su3.length]) scores[2]++;
+    });
+    
+    // 최고 점수를 찾음
+    const maxScore = Math.max(...scores);
+    
+    // 최고 점수를 받은 수포자를 결과 배열에 추가
+    let result = [];
+    scores.forEach((score, index) => {
+        if (score === maxScore) {
+            result.push(index + 1);
         }
-    }
-    answer.sort();
-    return answer;
+    });
+    
+    // 결과를 정렬하고 반환
+    result.sort();
+    return result;
 }
