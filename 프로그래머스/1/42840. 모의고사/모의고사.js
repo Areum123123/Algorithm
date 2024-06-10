836d99fed03c5d600b7fc986e128909afa85c99f
Areum <kang10232@naver.com>
2024-06-10 09:26:26 +0900

    [level 1] Title: 모의고사, Time: 6.13 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,17 +1,17 @@
 function solution(answers) {
     var answer = [];
-    var su1 = [1,2,3,4,5];
-    var su2 = [2,1,2,3,2,4,2,5];
-    var su3 = [3,3,1,1,2,2,4,4,5,5];
-    var sumarr = [0,0,0];
+    const su1 = [1,2,3,4,5];
+    const su2 = [2,1,2,3,2,4,2,5];
+    const su3 = [3,3,1,1,2,2,4,4,5,5];
+    let sumarr = [0,0,0];
   
-    for(var i=0; i<answers.length; i++){
+    for(let i=0; i<answers.length; i++){
         if(answers[i]==su1[i%su1.length]) sumarr[0]++;
         if(answers[i]==su2[i%su2.length]) sumarr[1]++;
         if(answers[i]==su3[i%su3.length]) sumarr[2]++;
     }
-    var maxValue = Math.max(...sumarr);
-    for(var j=0; j<sumarr.length; j++){
+    let maxValue = Math.max(...sumarr);
+    for(let j=0; j<sumarr.length; j++){
         if(sumarr[j] == maxValue){
             answer.push(j+1);
         }
