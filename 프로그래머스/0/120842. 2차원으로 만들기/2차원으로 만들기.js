e03331c30e167c29c92ea1979515eb330a78cfe4
Areum <kang10232@naver.com>
2024-06-11 09:19:15 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -0,0 +1,12 @@
+function solution(num_list, n) {
+    let result =[];
+    for(let i = 0 ; i < num_list.length;){
+        let array = [];
+        for(j = 0; j <n; j++){
+            array.push(num_list[i]);
+            i++
+        }
+        result.push(array);
+    }
+    return result;
+}
