d12a8840bb08ea6c3f0bd4471af065e8ca342052
Areum <kang10232@naver.com>
2024-06-12 09:27:12 +0900

    [level 0] Title: k의 개수, Time: 5.22 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120887. k의 개수/k의 개수.js
@@ -0,0 +1,12 @@
+function solution(i, j, k) {
+  let result =0;
+    for(let x = i; x <= j; x++){
+        let strX = String(x);
+      for(let y =0; y <strX.length; y++){
+       if(strX[y] === String(k)){
+           result ++;
+       }    
+      }  
+    }
+    return result;
+}
