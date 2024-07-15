c08e6c3d38f3d120a3afc5a463cbe8ce0238c869
Areum <kang10232@naver.com>
2024-07-15 09:29:23 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -1,9 +1,9 @@
 function solution(my_str, n) {
     let answer = []
   for(let i = 0; i < my_str.length; i+=n){
-      let cutString = my_str.substr(i , n)
-      console.log(cutString)
-     answer.push(cutString);
+   
+      answer.push(my_str.substr(i , n)) 
+     
   }
 
  return answer;     
