9a366c9273eea13ed3c5f8c14dd4ce8466aa9de0
Areum <kang10232@naver.com>
2024-07-17 09:48:32 +0900

    [level 0] Title: 문자열 계산하기, Time: 5.68 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -1,4 +1,25 @@
 function solution(my_string) {
-  
- return eval(my_string)
+    const array = my_string.split(' ')
+    console.log(array)
+    
+    let result = Number(array[0])
+     
+    for(let i = 1; i < array.length; i+=2){
+        const num = Number(array[ i + 1]);
+        const operator = array[i];
+        
+        switch(operator){
+        
+         case '+':
+         result += num;
+         break;
+                
+         case '-':
+         result -= num;
+          break;      
+        }
+        
+        
+    }
+    return result
 }
